d91bd44bb59712743a3523aa8297684b1ea01dce
nakster <g00332403@gmit.ie>
2019-02-09 18:13:30 +0000

    fixing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,24 +11,6 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # @all_ratings = Movie.ratings
-    # @sort = params[:sort] || session[:sort] 
-    # session[:ratings] = session[:ratings] || {'G'=>'','PG'=>'','PG-13'=>'','R'=>''}
-    # @t_param = params[:ratings] || session[:ratings]
-    # session[:sort] = @sort
-    # session[:ratings] = @t_param 
-   
-    # @selected_ratings = params[:ratings] || session[:ratings] || {}
-    # sort = params[:sort] || session[:sort]
-    
-    # if(params[:sort].nil? and !(session[:sort].nil?)) or (params[:ratings].nil? and !(session[:rating].nil?))
-    # flash.keep
-    # #redirect_to movies_path(sort: session[:sort], ratings: session[:ratings])
-    # redirect_to :sort => sort, :ratings => @selected_ratings and return
-    # end
-    
-    # @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
-    
     if(!params.has_key?(:sort) && !params.has_key?(:ratings))
       if(session.has_key?(:sort) || session.has_key?(:ratings))
         redirect_to movies_path(:sort=>session[:sort], :ratings=>session[:ratings])
@@ -48,7 +30,7 @@ class MoviesController < ApplicationController
         ratings = session[:ratings].keys
       end
     end
-    # @movies = Movie.order(@sort).find_or_create_by(ratings)
+   
     @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
     @mark  = ratings
   end
